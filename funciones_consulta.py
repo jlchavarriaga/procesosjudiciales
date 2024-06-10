8290ff238ad81e33431a22bd53774be25979e7c9
Jorge Chavarriaga <jlchavarriaga@gmail.com>
2024-06-10 14:29:41 -0500

    feat: get details for process

--- funciones_consulta.py
+++ funciones_consulta.py
@@ -87,6 +87,7 @@ def obtener_registros(pagina, tamano, cedula_actor='', cedula_demandado=''):
         return None
 
 
+# No implementado por seguridad
 def obtener_todos_los_registros(cedula_actor='', cedula_demandado=''):
     total_registros = obtener_numero_registros(cedula_actor, cedula_demandado)
     if total_registros == 0:
@@ -127,6 +128,31 @@ def obtener_todos_los_registros(cedula_actor='', cedula_demandado=''):
     }
 
     response = requests.post(url, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()  # Ajusta esto según el formato de la respuesta
+    else:
+        print('Error en la solicitud:', response.status_code)
+        print('Detalle:', response.text)
+        return None
+
+
+def obtener_detalle_registro(id_juicio):
+    url = f'https://api.funcionjudicial.gob.ec/EXPEL-CONSULTA-CAUSAS-CLEX-SERVICE/api/consulta-causas-clex/informacion/getIncidenteJudicatura/{id_juicio}'
+    headers = {
+        'Accept': 'application/vnd.api.v1+json',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'es-419,es-US;q=0.9,es;q=0.8,en;q=0.7,pt;q=0.6',
+        'Connection': 'keep-alive',
+        'Host': 'api.funcionjudicial.gob.ec',
+        'Referer': 'https://procesosjudiciales.funcionjudicial.gob.ec/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1'
+    }
+
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         return response.json()  # Ajusta esto según el formato de la respuesta
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import time
+from funciones_consulta import obtener_numero_registros, obtener_registros, obtener_detalle_registro
 
-from funciones_consulta import obtener_numero_registros, obtener_registros
 
-
-def main(cedula_actor='', cedula_demandado=''):
+def obtener_registros_con_detalles(cedula_actor='', cedula_demandado=''):
     total_registros = obtener_numero_registros(cedula_actor, cedula_demandado)
     if total_registros == 0:
         print("No se encontraron registros.")
-        return
+        return None
 
     tamano_pagina = 10
     total_paginas = (total_registros + tamano_pagina - 1) // tamano_pagina  # Redondeo hacia arriba
@@ -15,17 +14,38 @@ def main(cedula_actor='', cedula_demandado=''):
     print(f"Total de registros: {total_registros}")
     print(f"Total de páginas: {total_paginas}")
 
+    registros_con_detalles = []
+
     for pagina in range(1, total_paginas + 1):
         registros = obtener_registros(pagina, tamano_pagina, cedula_actor, cedula_demandado)
         if registros is not None:
-            print(f"Página {pagina}:")
-            print(registros)
+            print(f"Obteniendo detalles de la página {pagina}...")
+            for registro in registros:
+                id_juicio = registro.get('idJuicio')
+                if id_juicio:
+                    detalles = obtener_detalle_registro(id_juicio)
+                    if detalles:
+                        registro['detalles'] = detalles
+                        registros_con_detalles.append(registro)
+                    else:
+                        print(f"Error al obtener detalles para el idJuicio {id_juicio}")
+                else:
+                    print("El registro no tiene idJuicio")
+
+            time.sleep(2)  # Pausa de 2 segundos entre cada solicitud
         else:
             print(f"Error al obtener la página {pagina}")
 
-        time.sleep(2) # Pausa de 2 segundos entre cada solicitud
+    return registros_con_detalles
 
 if __name__ == "__main__":
     cedula_actor = '0968599020001'
     cedula_demandado = ''
-    main(cedula_actor, cedula_demandado)
+
+    registros_con_detalles = obtener_registros_con_detalles(cedula_actor, cedula_demandado)
+    if registros_con_detalles:
+        print("Registros con detalles obtenidos exitosamente:")
+        for registro in registros_con_detalles:
+            print(registro)
+    else:
+        print("No se pudieron obtener registros con detalles.")
